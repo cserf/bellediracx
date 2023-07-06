6b265167f0b956a5d3d6c2b0aa65f7a00561ed3b
serfon <cedric.serfon@desy.de>
2023-07-06 07:15:01 +0200

    Define more methods

--- src/bellediracx/routers/b2ruciodatamanager.py
+++ src/bellediracx/routers/b2ruciodatamanager.py
@@ -16,6 +16,7 @@ from diracx.core.config import Config, ConfigSource
 from diracx.routers.auth import UserInfo, has_properties, verify_dirac_token
 
 from diracx.routers.fastapi_classes import DiracxRouter
+from bellediracx.db.rucio.db import RucioDB
 
 LAST_MODIFIED_FORMAT = "%a, %d %b %Y %H:%M:%S GMT"
 
@@ -31,12 +32,37 @@ class LFNParams(BaseModel):
 
 
 @router.get("/{datablock}")
-async def get_single_datablock(datablock: str):
+async def get_single_datablock(
+    datablock: str,
+    user_info: Annotated[UserInfo, Depends(verify_dirac_token)],
+    rucio_db: Annotated[RucioDB, Depends(RucioDB.transaction)],
+):
     return f"This datablock {datablock}"
+
+@router.get("/staging/{lpn}")
+async def insert_lpn(
+    lpn: str,
+#    user_info: Annotated[UserInfo, Depends(verify_dirac_token)],
+    rucio_db: Annotated[RucioDB, Depends(RucioDB.transaction)],
+):
+    print(lpn)
+    rucio_db.search(lpn)
+    return f"This list of LFNs {lpn}"
+
+@router.post("/staging/{lpn}")
+async def insert_lpn(
+    lpn: str,
+#    user_info: Annotated[UserInfo, Depends(verify_dirac_token)],
+    rucio_db: Annotated[RucioDB, Depends(RucioDB.transaction)],
+):
+    print(lpn)
+    rucio_db.insert(lpn)
+    return f"This list of LFNs {lpn}"
 
 @router.post("/staging/status")
 async def get_multiple_file_status(
     user_info: Annotated[UserInfo, Depends(verify_dirac_token)],
+    rucio_db: Annotated[RucioDB, Depends(RucioDB.transaction)],
     body: LFNParams,
 ):
     lfns = body.lfns
